c2ca914d7c3df0e9ed63c71aaf58ec354c3d7a93
huynh.phat <huynh.phat@rivercrane.vn>
2023-11-18 23:14:39 +0700

    add package arcanedev/log-viewer

--- html/app/Providers/AppServiceProvider.php
+++ html/app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         URL::forceRootUrl('http://localhost:9001');
+
+        Paginator::useBootstrap();
     }
 }
